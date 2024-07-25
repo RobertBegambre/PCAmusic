821b4d4c319a0ee8cac7e0d22e11323bff09a767
RobertBegambre <robertjmeza12@gmail.com>
2024-07-25 15:09:02 -0500

    swiper container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {register} from 'swiper/Element/bundle';
+register();
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
