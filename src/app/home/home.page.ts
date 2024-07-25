d976320cbf46395c0dcf30cbad2fb0f0a703e79c
RobertBegambre <robertjmeza12@gmail.com>
2024-07-25 17:52:38 -0500

    actualizacion de home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Icon } from 'ionicons/dist/types/components/icon/icon';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slides = [
+    {
+      Title: "titulo de slide 1",
+      icon:"diamond-outline",
+      avatar:"https://as2.ftcdn.net/v2/jpg/05/79/49/49/1000_F_579494977_SJqiMuqaZX72bNNg1jkfoItRK8miicGQ.jpg",
+      imagen:"https://cdn.shopify.com/s/files/1/0268/6861/files/Gran-Danes_grande.jpg?v=1535218743",
+      descripcion:"texto de descipcion",
+    }
+  ]
   constructor() {}
 
 }
